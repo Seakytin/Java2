06701e033ad3a9ff734b1a6d930a61ca1bf6d37f
Seakytin <Seakytin@yandex.ru>
2021-02-11 19:36:53 +0300

    lesson5

--- homework/java2/lesson4/Test.java
+++ homework/java2/lesson4/Test.java
@@ -1,41 +1,33 @@
-package homework.java2.lesson4;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.function.Predicate;
-
-public class Test {
-    public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1, 2, 3 , 4, 5);
-       for (Integer x : list) {
-           if (x == null) {
-               System.out.println(-1);
-           } else {
-               System.out.println(x);
-               return;
-
-       }
-
-      // List<Integer> l = list.stream()
-      //         //.limit(1)
-      //         .collect(Collectors.toList(x -> ));
-      // System.out.println(l);
-      // public int search(Integer l, Integer[] list){
-      //     for (Integer x : list) {
-      //                 if (x == null) {
-      //                     System.out.println(-1);
-      //                 } else {
-      //                     System.out.println(x);
-      //                     return;
-
-                    }
-
-  }
-
-
-
-//   public int search(Integer n, Integer[] list){
-//   }
+//package homework.java2.lesson4;
 //
+//import java.lang.reflect.Array;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//import java.util.function.Predicate;
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        //        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+////        System.out.println(search(-10, list1));
+//
+//        Integer[] list1 = {1, 2, 3, 4, 5};
+//
+//        search(list1,
+//                n -> {
+//                    if ( != 1)
+//
+//                });
+//
+//
+//    }
+//
+//    public static void search(Integer n, Integer[] list, CustomInterface<Integer> customInterface) {
+//        for (int i = 0; i < list.length; i++) {
+//            if (customInterface.operation(n, list[i])) {
+//
+//            }
+//        }
+//    }
+//}
